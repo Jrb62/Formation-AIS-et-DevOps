704a661c57f6279ab6ac0eceb8327cb3dea420ba
Jrb62 <98950407+Jrb62@users.noreply.github.com>
2023-11-15 15:58:10 +0100

    Ajout du script Quantique

---
+++ quantique.py
@@ -0,0 +1,31 @@
+import pyspx.shake_128f as sphincs
+import os
+
+# Génération de clés : clé privée + clé publique
+seed = os.urandom(sphincs.crypto_sign_SEEDBYTES)
+public_key, secret_key = sphincs.generate_keypair(seed)
+
+# Signature du message et vérification de la signature
+message = b"Tony mon ami pour la vie"
+signature = sphincs.sign(message, secret_key)
+valid = sphincs.verify(message, signature, public_key)
+
+print("Message:", message)
+print("Signature valide?", valid)
+
+# Vérification d'un message modifié + signature
+message_modifie = message
+valid_modifie = sphincs.verify(message_modifie, signature, public_key)
+
+print("Message modifié:", message_modifie)
+
+
+
+
+
+
+
+
+
+
+
